76ccb049e8aa8e9ad8f89e6d24292b1d8109e362
anix782 <makartchapin@yandex.ru>
2026-07-09 21:53:10 +0400

    сначала он спит когда нажимаешь на логотип он весёлый

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.Happy)
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.Sad)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.Asleep)
 })
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
 basic.showIcon(IconNames.Asleep)
