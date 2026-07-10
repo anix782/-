b4a2ddc93e29baaf1a01fe2c796a65653c546ebb
anix <makartchapin@yandex.ru>
2026-07-10 14:32:56 +0400

    теперь его можно кормить его просто нажать  букву в правее

--- main.ts
+++ main.ts
@@ -2,9 +2,31 @@ input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.Angry)
 })
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # . # .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # . # #
+        . # # # .
+        `)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Asleep)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
